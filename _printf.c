d998e84d1b2767a00ec6ca195837dbffe4135c6c
Lucas <14808@holbertonstudnets.com>
2026-08-10 02:25:04 +0000

    edited name

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _printf - handles c, s, and %% conversions
+ * @format: the format string
+ *
+ * Return: the total number of characters written
+ */
+int	_printf(const char *format, ...)
+{
+	va_list	args;
+	int		count;
+	int		i;
+
+	if (!format)
+		return (-1);
+	va_start(args, format);
+	count = 0;
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c')
+				count += handle_char(args);
+			else if (format[i] == 's')
+				count += handle_string(args);
+			else if (format[i] == '%')
+				count += handle_percent();
+		}
+		else
+			count += putchar_out(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
