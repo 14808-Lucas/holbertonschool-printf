1143ce0ae24ec457ad915b0b615ba4fd65ba70af
Lucas <14808@holbertonstudnets.com>
2026-08-10 00:14:35 +0000

    function that handles %%

---
+++ handle_percent.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * handle_percent - writes a literal '%' to standard output
+ *
+ * Return: 1 on success, -1 on write failure
+ */
+int	handle_percent(void)
+{
+	return (putchar_out('%'));
+}
